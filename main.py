4b76ad412f451719bea60fedb344b477dcce6f6c
Sandra <sandrav0609@gmail.com>
2018-05-08 08:23:08 -0400

    Agrega objeto tarifa

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from datetime import datetime, date, time, timedelta
 import calendar
+from tarifa import tarifa
 
 def calcularPrecio(tarifa, tiempoDeServicio):
+    #tiempoInicio.tm_hour=0
+    #tiempoInicio.tm_min=0
+    #tiempoInicio.tm_sec=0
+    
+    #Calculo de horas
+    if(tiempoDeServicio[0]==tiempoDeServicio[1]): #Menos de una hora
+        pago=tarifa.
+    else:
+        pago=
+    
+     
+    
+    
     
---
+++ tarifa.py
@@ -0,0 +1,8 @@
+class tarifa(object):
+    semana=0
+    finDeSemana=0
+    
+    def __init__(self,tarifaSemana,tarifaFinDeSemana):
+        self.semana=tarifaSemana
+        self.finDeSemana=tarifaFinDeSemana
+        
