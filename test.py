ae0936d565ce21de2cc4941d8907dd8324cd73ac
Elvin Quero <elvinempor@gmail.com>
2018-05-09 18:56:44 -0400

    Clasificacion casos [2.0]

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from tiempoDeTrabajo import tiempoDeTrabajo
 from main import calcularPrecio
 
 
-class test_calcularPrecio(unittest.TestCase):
+class test_calcularPrecio_frontera(unittest.TestCase):
 
 	def setUp(self):
 		self.tarifa = tarifa(15,19)
@@ -21,12 +21,49 @@ class test_calcularPrecio(unittest.TestCase):
 		res = calcularPrecio(self.tarifa, tiempoDeServicio)
 		self.assertEqual(res, 15, "Espera 15. Dio " + str(res) )
 
+
+
+
+	def test_casi7Dias(self):
+		#COMIENZO UN LUNES, TERMINO UN SABADO A LAS 23:59 6 Dias.
+		inicio = tiempoDeTrabajo(2018,5,7,0,0,0)
+		final = tiempoDeTrabajo(2018,5,12,23,59,0)
+		tiempoDeServicio = [inicio, final]
+		res = calcularPrecio(self.tarifa, tiempoDeServicio)
+		self.assertEqual(res, 2256, "Espera 2256. Dio " + str(res) )
+
+
+class test_calcularPrecio_equinas(unittest.TestCase):
+
+	def setUp(self):
+		self.tarifa = tarifa(15,19)
+
+	def test_14min(self):
+		inicio = tiempoDeTrabajo(2018,5,7,0,0,0)
+		final = tiempoDeTrabajo(2018,5,7,0,14,0)
+		tiempoDeServicio = [inicio, final]
+		#res = calcularPrecio(self.tarifa, tiempoDeServicio)
+		self.assertRaises(Exception, calcularPrecio, self.tarifa, tiempoDeServicio)
+
+	def test_8Dias(self):
+		inicio = tiempoDeTrabajo(2018,5,7,0,0,0)
+		final = tiempoDeTrabajo(2018,5,14,0,0,0)
+		tiempoDeServicio = [inicio, final]
+		#res = calcularPrecio(self.tarifa, tiempoDeServicio)
+		self.assertRaises(Exception, calcularPrecio, self.tarifa, tiempoDeServicio)
+		#self.assertEqual(res, 2712, "Espera 2712. Dio " + str(res) )
+
+
+
+class test_calcularPrecio_malicia(unittest.TestCase):
+	def setUp(self):
+		self.tarifa = tarifa(15,19)
+
 	def test_mismoDia(self):
 		inicio = tiempoDeTrabajo(2018,5,9,2,50,53)
 		final = tiempoDeTrabajo(2018,5,9,3,50,53)
 		tiempoDeServicio = [inicio, final]
-		#tiempoDeServicio[0] = inicio
-		#tiempoDeServicio[1] = final
+
 
 		self.assertEqual(calcularPrecio(self.tarifa, tiempoDeServicio), 15, "test_mismoDia fallo")
 
@@ -57,37 +94,10 @@ class test_calcularPrecio(unittest.TestCase):
 
 	def test_tresDias(self):
 		inicio = tiempoDeTrabajo(2018,5,7,3,50,53)
-		final = tiempoDeTrabajo(2018,5,10,3,50,52)
+		final = tiempoDeTrabajo(2018,5,10,3,50,53)
 		tiempoDeServicio = [inicio, final]
 		res = calcularPrecio(self.tarifa, tiempoDeServicio)
 		self.assertEqual(res, 1080, "Espera 1080. Dio " + str(res) )
-
-
-	def test_casi7Dias(self):
-		#COMIENZO UN LUNES, TERMINO UN SABADO A LAS 23:59 6 Dias.
-		inicio = tiempoDeTrabajo(2018,5,7,0,0,0)
-		final = tiempoDeTrabajo(2018,5,12,23,59,0)
-		tiempoDeServicio = [inicio, final]
-		res = calcularPrecio(self.tarifa, tiempoDeServicio)
-		self.assertEqual(res, 2256, "Espera 2256. Dio " + str(res) )
-
-
-	def test_8Dias(self):
-		inicio = tiempoDeTrabajo(2018,5,7,0,0,0)
-		final = tiempoDeTrabajo(2018,5,14,0,0,0)
-		tiempoDeServicio = [inicio, final]
-		#res = calcularPrecio(self.tarifa, tiempoDeServicio)
-		self.assertRaises(Exception, calcularPrecio, self.tarifa, tiempoDeServicio)
-		#self.assertEqual(res, 2712, "Espera 2712. Dio " + str(res) )
-
-	def test_14min(self):
-		inicio = tiempoDeTrabajo(2018,5,7,0,0,0)
-		final = tiempoDeTrabajo(2018,5,7,0,14,0)
-		tiempoDeServicio = [inicio, final]
-		#res = calcularPrecio(self.tarifa, tiempoDeServicio)
-		self.assertRaises(Exception, calcularPrecio, self.tarifa, tiempoDeServicio)
-
-	def test_finItermedio(self):
 
 
 
@@ -95,17 +105,20 @@ def suite():
     suite = unitest.TestSuite()
 
     #Fronteras
-    suite.addTest(test_calcularPrecio('test_15min'))
-    suite.addTest(test_calcularPrecio('test_14min'))
-    suite.addTest(test_calcularPrecio('test_8Dias'))
+    suite.addTest(test_calcularPrecio_frontera('test_15min'))
+    suite.addTest(test_calcularPrecio_frontera('test_casi7Dias'))
 
-    suite.addTest(test_calcularPrecio('test_trabajoUnaHoraFin'))
+    #Equinas
+    suite.addTest(test_calcularPrecio_equinas('test_14min'))
+    suite.addTest(test_calcularPrecio_equinas('test_8Dias'))
 
-    suite.addTest(test_calcularPrecio('test_trabajoDosHoras'))
-    suite.addTest(test_calcularPrecio('test_menosDeUnaHora'))
-    suite.addTest(test_calcularPrecio('test_mismoDia'))
-    suite.addTest(test_calcularPrecio('test_tresDias'))
-    suite.addTest(test_calcularPrecio('test_casi7Dias'))
+    #malicia
+    suite.addTest(test_calcularPrecio_malicia('test_trabajoUnaHoraFin'))
+    suite.addTest(test_calcularPrecio_malicia('test_trabajoDosHoras'))
+    suite.addTest(test_calcularPrecio_malicia('test_menosDeUnaHora'))
+    suite.addTest(test_calcularPrecio_malicia('test_mismoDia'))
+    suite.addTest(test_calcularPrecio_malicia('test_tresDias'))
+
 
 
     return suite
